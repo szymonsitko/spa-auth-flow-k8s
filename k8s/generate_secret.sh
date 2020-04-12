17b15a3de7f2feb298c038dd30a2ee9a8c37901f
simon <ssitko@pgs-soft.com>
2020-04-12 09:28:51 +0200

    Added k8s definition

---
+++ k8s/generate_secret.sh
@@ -0,0 +1,8 @@
+# Assign environmental variables
+ENV_VAR_VALUE=$2
+
+# Create secret
+kubectl create secret generic secretkey --from-literal SECRET_KEY=$ENV_VAR_VALUE
+
+# Describe newly created secret
+kubectl describe secret secretkey
